50e13090d690025b5715247d98cf8be537c66322
dhrupo <dhrupo@gmail.com>
2021-02-08 10:55:59 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,59 @@
+const searchSong = async () => {
+  const searchText = document.getElementById('search-field').value;
+  document.getElementById('song-lyrics').innerText = "";
+  try {
+    const res = await fetch(`https://api.lyrics.ovh/suggest/${searchText}`);
+    const data = await res.json();
+    displaySongs(data.data);
+  }
+  catch (error) {
+    displayError();
+  }
+}
+
+const displaySongs = songs => {
+  const songContainer = document.getElementById('song-container');
+  songContainer.innerText = "";
+  if (songs.length === 0) {
+    displayError();
+  }
+  else {
+    songs.forEach(song => {
+      const songDiv = document.createElement("div");
+      document.getElementById('error').innerText = "";
+      songDiv.className = "single-result row align-items-center my-3 p-3";
+      songDiv.innerHTML = `
+          <div class="col-md-9">
+            <h3 class="lyrics-name">${song.title}</h3>
+            <p class="author lead">Album by <span>${song.artist.name}</span></p>
+            <audio controls>
+              <source src="${song.preview}" type="audio/mpeg">
+            </audio>
+          </div>
+          <div class="col-md-3 text-md-right text-center">
+            <button onclick="getLyric('${song.artist.name}', '${song.title}')" class="btn btn-success">Get Lyrics</button>
+          </div>
+        `;
+      songContainer.appendChild(songDiv);
+    });
+  }
+}
+
+const getLyric = (artist, title) => {
+  fetch(`https://api.lyrics.ovh/v1/${artist}/${title}`)
+    .then(res => res.json())
+    .then(data => displayLyrics(data.lyrics))
+    .catch(error => displayError())
+}
+
+const displayLyrics = lyrics => {
+  const lyricsDiv = document.getElementById('song-lyrics');
+  lyricsDiv.innerText = "";
+  lyricsDiv.innerText = lyrics;
+}
+
+const displayError = () => {
+  document.getElementById('error').innerText = "Something went wrong. Please try again";
+}
+
+document.getElementById('search').addEventListener('click', searchSong);
